8ff6c5fb7afc4e8c90dca2a59ff1399531031c95
Sergei Grinko <gs.html.developer@gmail.com>
2020-06-18 10:56:51 +0300

    mobile

--- js/common.js
+++ js/common.js
@@ -3,6 +3,29 @@ $(document).ready(function () {
 		arrows:true,
 		slidesToShow: 5,
   		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 	$('.top-slider').slick({
 		arrows:false,
@@ -16,7 +39,7 @@ $(document).ready(function () {
 	$('.product-slider').slick({
 		arrows:false,
 		slidesToShow: 4,
-  		slidesToScroll: 4,
+  		slidesToScroll: 1,
   		vertical: true,
   		verticalSwiping: true,
   		asNavFor: '.product-center',
@@ -28,5 +51,18 @@ $(document).ready(function () {
   		slidesToScroll: 1,
   		asNavFor: '.product-slider',
 
+	});
+
+	$('.menu-btn').on('click', function(e) {
+		e.preventDefault();
+		$('.header .header-bottom').addClass('active');
+		$('.header').append('<div class="shadow"></div>');
+	});
+	$(document).mouseup(function (e){
+		var div = $(".header .header-bottom");
+		if (!div.is(e.target) && div.has(e.target).length === 0) {
+			$('.header .header-bottom').removeClass('active');
+			$('.shadow').remove();
+		}
 	});
 });
